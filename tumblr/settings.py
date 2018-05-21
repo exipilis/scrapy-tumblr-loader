f95af651ad180f393ffc772200d748900ceccfbe
exipilis <malenko.andrii@gmail.com>
2018-05-21 22:16:22 +0300

    user agent

--- tumblr/settings.py
+++ tumblr/settings.py
@@ -15,7 +15,7 @@ SPIDER_MODULES = ['tumblr.spiders']
 NEWSPIDER_MODULE = 'tumblr.spiders'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-# USER_AGENT = 'tumblr (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
